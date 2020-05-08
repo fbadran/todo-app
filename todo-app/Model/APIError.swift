ffb520bd23edac9a238e149f547ff24b2b939cea
Faisal <faisalbadran1@gmail.com>
2020-05-08 13:45:47 -0400

    Revert "Revert "Project commit ""
    This reverts commit 79e400f53658b85c678490c593739e72d9c2f966.

---
+++ todo-app/Model/APIError.swift
@@ -0,0 +1,13 @@
+//
+//  APIError.swift
+//  todo-app
+//
+//  Created by faisal badran on 2020-05-05.
+//  Copyright © 2020 faisal badran. All rights reserved.
+//
+
+import Foundation
+
+struct APIError: Codable {
+    let message: String
+}
